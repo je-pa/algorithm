9062fad03cba394574c656b76a5b21863971359b
je-pa <76720692+je-pa@users.noreply.github.com>
2023-07-26 15:06:05 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int price) {
-        int answer = 0;
-        return answer;
+        int p = price/100_000;
+        // return (int)(p >= 5 ? price*0.8 : p >= 3 ? price*0.9 : p>=1 ? price*0.95 : price);
+        return (int)(price*(p >= 5 ? 0.8 : p >= 3 ? 0.9 : p>=1 ? 0.95 : 1));
     }
 }
