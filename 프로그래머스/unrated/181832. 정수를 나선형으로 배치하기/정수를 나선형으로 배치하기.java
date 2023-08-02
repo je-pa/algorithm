1ab84b5bcc165f6ec07581a91a56fe52ca001efe
je-pa <76720692+je-pa@users.noreply.github.com>
2023-08-02 16:56:30 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.11 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[][] solution(int n) {
+        int[][] arr = new int[n][n];
+        int i=1;
+        int x=0, y=0;
+        int[][] d = {{0,1},{1,0},{0,-1},{-1,0}};
+        int cd = 0;
+        while(i<n*n+1){
+            arr[x][y] = i;
+            
+            int cdx = d[cd][0];
+            int cdy = d[cd][1];
+            if(x+cdx<0 || y+cdy<0 || x+cdx>=n || y+cdy>=n || arr[x+cdx][y+cdy] != 0){
+                cd = (cd+1)%4;  
+            }
+            x += d[cd][0];
+            y += d[cd][1];
+            i++;
+        }
+        return arr;
+    }
+}
