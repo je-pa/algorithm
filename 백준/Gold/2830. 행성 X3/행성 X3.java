e5ebee431e85278a32d803ba1bc80ec17c77161f
je-pa <76720692+je-pa@users.noreply.github.com>
2023-08-14 21:10:44 +0900

    [Gold III] Title: 행성 X3, Time: 408 ms, Memory: 68544 KB -BaekjoonHub

--- 백준/Gold/2830. 행성 X3/행성 X3.java
+++ 백준/Gold/2830. 행성 X3/행성 X3.java
@@ -2,30 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    private static final int DIGIT = 20;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] arr= new int[n];
         int max = 0;
-
+       
         for(int i = 0 ; i<n ; i++){
             arr[i] = Integer.parseInt(br.readLine());
             max = Math.max(arr[i], max);
         }
+        long ss = 1L;
         long answer = 0;
-        int k=0;
         while(max != 0){
-            int count1 = 0;
+            long count1 = 0;
             for(int i=0; i <arr.length ;i++){
                 if(arr[i] == 0)continue;
-                if((arr[i] & (1 << k)) == 0) count1++;
-            }
-            answer += (1L << k) * count1 * (n - count1);
+                if(arr[i]%2 == 1) count1++;
+                arr[i] /= 2;
+            }    
+            answer += (ss * count1 * (n - count1));
+            // answer += (((long)count1 * (n - count1)) * (long)ss);
+            ss = ss << 1;
             max/=2;
-            k++;
         }
-
-        System.out.print(arr.length==1 ? arr[0] : answer);
+        
+        System.out.print(answer);
     }
 }
