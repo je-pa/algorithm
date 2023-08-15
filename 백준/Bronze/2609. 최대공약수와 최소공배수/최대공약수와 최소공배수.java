ff4bce9786f017092dffadb2d1db5e176266de79
je-pa <76720692+je-pa@users.noreply.github.com>
2023-08-16 01:08:14 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 156 ms, Memory: 16308 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int gcf=greatestCommonFactor(arr[0],arr[1]);
+        System.out.println(gcf+"\n"+arr[0]/gcf*arr[1]);
+    }
+    static int greatestCommonFactor(int x, int y){
+        if(x == 0 ) return y;
+        else return greatestCommonFactor(y%x,x);
+    }
+}
