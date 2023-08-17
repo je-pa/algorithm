25ff1e98ac0a734d201961bf9706f1159244832a
je-pa <76720692+je-pa@users.noreply.github.com>
2023-08-17 20:15:21 +0900

    [Silver II] Title: 에디터, Time: 620 ms, Memory: 85004 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<String> left = new Stack();
+        Stack<String> right = new Stack<>();
+        String[] strArr = br.readLine().split("");
+        for(String s : strArr){
+            left.push(s);
+        }
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n ; i++){
+            String str = br.readLine();
+            switch (str.charAt(0)){
+                case 'L':
+                    if(!left.empty()) {
+                        right.push(left.pop());
+                    }
+                    break;
+                case 'D':
+                    if(!right.empty()){
+                        left.push(right.pop());
+                    }
+                    break;
+                case 'B':
+                    if(!left.empty()) left.pop();
+                    break;
+                case 'P':
+                    left.push(Character.toString(str.charAt(2)));
+                    break;
+            }
+        }
+        while(!left.empty()){
+            right.push(left.pop());
+        }
+        while(!right.empty()){
+            sb.append(right.pop());
+        }
+        System.out.println(sb);
+    }
+}
