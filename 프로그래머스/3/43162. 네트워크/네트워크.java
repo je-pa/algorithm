5d9f120360ddc72dd9ead6e59cb7ecc675f0ddf3
je-pa <76720692+je-pa@users.noreply.github.com>
2023-12-15 21:54:21 +0900

    [level 3] Title: 네트워크, Time: 0.12 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        for(int i=0 ; i<n ; i++){
+            if(computers[i][i]==1){
+                answer++;
+                check(i,i,computers, n);
+            }    
+        }
+        return answer;
+    }
+    void check(int x , int y, int[][] computers, int n){
+        computers[x][x]=0;
+
+        for(int i=0 ; i<n ; i++){
+            if(computers[x][i]==1){
+                computers[x][i] = 0;
+                computers[i][x] = 0;    
+                check(i,i,computers, n);
+            }
+        }
+    }
+}
