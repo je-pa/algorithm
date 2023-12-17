7f50fe4ecd21229cd201b874518b2a79079808a7
je-pa <76720692+je-pa@users.noreply.github.com>
2023-12-17 20:12:48 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 164 ms, Memory: 22300 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -1,19 +1,19 @@
-import java.util.Scanner;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int[] o = new int[1_000_001];
-        o[1]=1;
-        o[2]=2;
-        o[3]=4;
-        for(int i=4; i<o.length ; i++){
-            o[i]=((o[i-3]+o[i-2])%1_000_000_009+o[i-1])%1_000_000_009;
-        }
-        for(int i=0 ; i<n ; i++){
-            int x = scan.nextInt();
-            System.out.println(o[x]);
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+        int n = stoi(br.readLine());
+        long dp[] = new long[1_000_001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int i = 4; i <= 1_000_000; i++)
+        	dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % 1_000_000_009;
+        for(int i = 0; i < n; i++){
+        	System.out.println(dp[stoi(br.readLine())]);
         }
     }
+	public static int stoi(String str) {return Integer.parseInt(str);}
 }
