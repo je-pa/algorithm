b6ab45967facf61d63e6106c6bfff8599311e38c
je-pa <76720692+je-pa@users.noreply.github.com>
2023-12-22 19:47:29 +0900

    [Bronze II] Title: 문자열 분석, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = null;
+        while((str=br.readLine()) != null){
+            int[] count = new int[4];
+            for(char c : str.toCharArray()){
+                if(c>='a'&&c<='z'){
+                    count[0]++;
+                }else if(c>='A'&&c<='Z'){
+                    count[1]++;
+                }else if(c==' '){
+                    count[3]++;
+                }else{
+                    count[2]++;
+                }
+            }
+            for(int i : count){
+                sb.append(i).append(' ');
+            }sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
