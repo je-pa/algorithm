913c3ebd9190af44043709cc8808c1b2ede5ecdf
je-pa <76720692+je-pa@users.noreply.github.com>
2023-12-27 00:22:33 +0900

    [Gold IV] Title: 이모티콘, Time: 220 ms, Memory: 21780 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.java
+++ 백준/Gold/14226. 이모티콘/이모티콘.java
@@ -1,59 +1,40 @@
-import java.util.*;
- 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class Main {
-    
-    static boolean[][] visited = new boolean[1001][1001];//[clipboard][total]
-    
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        int s = scan.nextInt();
-        
-        bfs(s);
-    }
-    
-    public static void bfs(int s) {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(0, 1, 0));
-        visited[0][1] = true; 
-        
-        while(!q.isEmpty()) {
-            Node current = q.poll();
-            
-            if(current.total == s) {
-                System.out.println(current.time);
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        Queue<Icon> queue = new LinkedList<>();
+        queue.add(new Icon(1,0,0));
+        boolean[][] visited = new boolean[1001][1001];
+        while (!queue.isEmpty()){
+            Icon cur = queue.remove();
+            if(cur.screen == N){
+                System.out.println(cur.count);
                 return;
             }
-            
-            // 1. 화면에 있는 이모티콘 클립보드에 저장
-            q.offer(new Node(current.total, current.total, current.time + 1)); 
-            
-            
-            // 2. 클립보드에 있는 이모티콘 붙여넣기. 
-            // 클립보드 비어있지 않아야하고, 붙여넣은 후 개수가 총 개수보다 적어야 하며, 이전에 방문한적 없어야함.
-            if(current.clipboard != 0 && current.total + current.clipboard <= s && !visited[current.clipboard][current.total + current.clipboard]) {
-                q.offer(new Node(current.clipboard, current.total + current.clipboard, current.time + 1));
-                visited[current.clipboard][current.total + current.clipboard] = true;
+            if(cur.screen+ cur.clip< visited.length && !visited[cur.clip][cur.screen+ cur.clip]){
+                queue.add(new Icon(cur.screen + cur.clip, cur.count+1, cur.clip));
+                visited[cur.clip][cur.screen + cur.clip] = true;
             }
-            
-            // 3. 화면에 있는 이모티콘 중 하나 삭제.
-            // 총 개수 1보다 크거나 같아야하고, 방문한적 없어야함.
-            if(current.total >= 1 && !visited[current.clipboard][current.total - 1]) {
-                q.offer(new Node(current.clipboard, current.total - 1, current.time + 1));
-                visited[current.clipboard][current.total - 1] = true;
+            queue.add(new Icon(cur.screen, cur.count+1, cur.screen));
+            if(cur.screen>=1 && !visited[cur.clip][cur.screen-1]){
+                queue.add(new Icon(cur.screen-1, cur.count+1, cur.clip));
+                visited[cur.clip][cur.screen-1] = true;
             }
         }
     }
-    
-    public static class Node {
-        int clipboard;
-        int total;
-        int time;
-        
-        public Node(int clipboard, int total, int time) {
-            this.clipboard = clipboard;
-            this.total = total;
-            this.time = time;
-        }
+}
+class Icon{
+    int screen;
+    int count;
+    int clip;
+
+    Icon(int screen, int count, int clip){
+        this.screen = screen;
+        this.count = count;
+        this.clip = clip;
     }
 }
