71bdc970dd6d860db6066cbdb54f77d50d0dc560
je-pa <76720692+je-pa@users.noreply.github.com>
2023-12-30 01:43:32 +0900

    [Gold V] Title: 트리, Time: 128 ms, Memory: 14432 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    static int N = 0;
+    static int[] arr;
+    static int REMOVE = 0;
+    static int ROOT = 0;
+    static int count = 0;
+    static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        REMOVE = Integer.parseInt(br.readLine());
+
+        for(int i=0 ; i<N ; i++){
+            list.add(new ArrayList<>());
+        }
+
+        for(int i=0 ; i<N ; i++){
+            if(arr[i]==-1){
+                ROOT = i;
+            }
+            if(i==REMOVE) continue;
+            if(arr[i]==-1) continue;
+            list.get(arr[i]).add(i);
+        }
+        if(ROOT == REMOVE){
+            System.out.println(0);
+            return;
+        }
+        dfs(ROOT);
+        System.out.println(count);
+    }
+    static void dfs(int index){
+        if(index != REMOVE && list.get(index).isEmpty()) {
+            count++;
+            return;
+        }
+        for(int i : list.get(index)){
+            dfs(i);
+        }
+    }
+}
