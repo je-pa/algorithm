894a4d9e66dbf82e5ef6cbb83bac06a10e382ba1
je-pa <76720692+je-pa@users.noreply.github.com>
2024-01-04 12:42:17 +0900

    [Silver I] Title: 트리 순회, Time: 124 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,83 +1,68 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static Map<String, Node> map = new HashMap<>();
+    static int N = 0;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-	static Node head = new Node('A', null, null);
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		
-		for(int i=0;i<n;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			char root = st.nextToken().charAt(0);
-			char left = st.nextToken().charAt(0);
-			char right = st.nextToken().charAt(0);
-			
-			insertNode(head, root,left,right);
-		}
-		
-		preOrder(head);
-		System.out.println();
-		inOrder(head);
-		System.out.println();
-		postOrder(head);
-		System.out.println();
-		
-		
-		
-	}
-	
-	
-	static class Node{
-		char value;
-		Node left;
-		Node right;
-		
-		Node(char value, Node left, Node right){
-			this.value = value;
-			this.left = left;
-			this.right = right;
-		}
-	}
+        for(int i =0  ; i<N ; i++){
+            String[] arr = br.readLine().split(" ");
+            map.put(arr[0],new Node(arr[0], arr[1], arr[2]));
+        }
+        rootLeftRight("A");
+        sb.append("\n");
+        leftRootRight("A");
+        sb.append("\n");
+        leftRightRoot("A");
+        System.out.println(sb);
+    }
 
-	public static void insertNode(Node temp, char root, char left, char right) {
-		
-		
-		if (temp.value == root) {
-			temp.left = (left == '.' ? null : new Node(left,null,null));
-			temp.right = (right == '.' ? null : new Node(right,null,null)); 
-		}
-		else {
-			if(temp.left != null) insertNode(temp.left, root, left, right);
-			if(temp.right != null) insertNode(temp.right, root, left, right);
-		}
-	}
-	
-	public static void preOrder(Node node) {
-		if(node ==null) return;
-		System.out.print(node.value);
-		preOrder(node.left);
-		preOrder(node.right);
-	}
-	
-	public static void inOrder(Node node) {
-		if(node ==null) return;
-		inOrder(node.left);
-		System.out.print(node.value);
-		inOrder(node.right);
-	}
-	
-	public static void postOrder(Node node) {
-		if(node ==null) return;
-		postOrder(node.left);
-		postOrder(node.right);
-		System.out.print(node.value);
-	}
+    static void rootLeftRight(String cur){
+        Node node = map.get(cur);
+        sb.append(node.root);
+        if(!node.left.equals(".")){
+            rootLeftRight(node.left);
+        }
+        if(!node.right.equals(".")){
+            rootLeftRight(node.right);
+        }
+    }
+    static void leftRootRight(String cur){
+        Node node = map.get(cur);
+        if(!node.left.equals(".")){
+            leftRootRight(node.left);
+        }
+        sb.append(node.root);
+        if(!node.right.equals(".")){
+            leftRootRight(node.right);
+        }
+    }
+    static void leftRightRoot(String cur){
+        Node node = map.get(cur);
+        if(!node.left.equals(".")){
+            leftRightRoot(node.left);
+        }
+        if(!node.right.equals(".")){
+            leftRightRoot(node.right);
+        }
+        sb.append(node.root);
+    }
 
+}
+class Node{
+    String root;
+    String left;
+    String right;
+
+    public Node(String root, String left, String right) {
+        this.root = root;
+        this.left = left;
+        this.right = right;
+    }
 }
