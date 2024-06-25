446985e474a3a5842afed380072df52cebf5c50e
je-pa <76720692+je-pa@users.noreply.github.com>
2024-06-25 15:48:19 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        long answer = 0;
+        while(n!=0){
+            answer+=n%3;
+            n/=3;
+            answer*=(long)10;
+        }
+        answer/=10;
+        int i=1;
+        int result = 0;
+        while(answer != 0){
+            result += (answer%10)*i;
+            answer/=10;
+            i*=3;
+        }
+        return result;
+    }
+}
