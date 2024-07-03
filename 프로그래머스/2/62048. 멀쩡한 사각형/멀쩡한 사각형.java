6f7f2fefe065d7beefa21020ac91e72b74b695bb
je-pa <76720692+je-pa@users.noreply.github.com>
2024-07-03 10:15:29 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.31 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,21 +1,21 @@
 class Solution {
     public long solution(int w, int h) {
         long answer = 1;
-        long k = gcd(w,h);
-        long wr = (long)((long)w/(long)k);
-        long hr = (long)((long)h/(long)k);
+        // long k = gcd(w,h);
+        // long wr = (long)((long)w/(long)k);
+        // long hr = (long)((long)h/(long)k);
         long count = 0;
-        for(long i=0 ; i<wr; i++){
-            long cy = (long)((double)hr/(double)wr*(double)i);
-            long ny = (long)(Math.ceil((double)hr/(double)wr*((double)i+1.0)));
-            count+=ny-cy;
-        }
-        // for(long i=0 ; i<w; i++){
-        //     long cy = (long)((double)h/(double)w*(double)i);
-        //     long ny = (long)(Math.ceil((double)h/(double)w*((double)i+1.0)));
+        // for(long i=0 ; i<wr; i++){
+        //     long cy = (long)((double)hr/(double)wr*(double)i);
+        //     long ny = (long)(Math.ceil((double)hr/(double)wr*((double)i+1.0)));
         //     count+=ny-cy;
         // }
-        return ((long)w*(long)h)-count*k;
+        for(long i=0 ; i<w; i++){
+            long cy = (long)((double)i*(double)h/(double)w);
+            long ny = (long)(Math.ceil((double)h/(double)w*((double)i+1.0)));
+            count+=ny-cy;
+        }
+        return ((long)w*(long)h)-count;
     }
     
     public long gcd(long x, long y){
