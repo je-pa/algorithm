d744867dca89ce6c55897b00b869a969b3748d1a
je-pa <76720692+je-pa@users.noreply.github.com>
2024-07-03 11:57:05 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,18 +1,12 @@
 class Solution {
     public int solution(int a, int b, int n) {
-        int answer = 0;
-        int count = 0;
-        
-//         while(){
-            
-//         }
-        return re(a,b,n);
+        return (n > b ? n - b : 0) / (a - b) * b;
     }
     
     public int re(int a , int b, int n){
         if(n < a){
             return 0;
         }
-        return (n/a)*b + re(a, b, ((n/a)*b) + (n%a));
+        return n/a*b + re(a, b, n/a*b + (n%a));
     }
 }
