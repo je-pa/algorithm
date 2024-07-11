0e5406f539ec592454787f2696a4cbe5f5eb0cd0
je-pa <76720692+je-pa@users.noreply.github.com>
2024-07-11 09:40:13 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(int a, int b) {
         String answer = "";
-        int[] arr = new int[]{31, 29, 31, 30, 31,30,31,31,30,31,30,31};
+        int[] arr = new int[]{31, 29, 31, 30, 31,30,31,31,30,31,30};
         String[] yo = new String[]{"THU","FRI","SAT","SUN","MON","TUE","WED"};
         int sum = 0;
         for(int i=0 ; i< a-1; i++){
