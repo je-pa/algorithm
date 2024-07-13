79c5ccd5a23e5dfcacc620853b78cb2ede9c4f3d
je-pa <76720692+je-pa@users.noreply.github.com>
2024-07-13 12:42:26 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 1391.97 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
@@ -1,21 +1,16 @@
 
 class Solution {
     public int solution(int n, long l, long r) {
-        int answer = 0;
-
-        for (long val = l; val <= r; val++) {
-            answer += query(n, val - 1);
+        long answer = r-l+1;
+        for(long i=l-1;i<=r-1;i++){
+            for(int j=0;j<n;j++){
+                if((i/(int)Math.pow(5,j))%5==2){
+                    answer--;
+                    break;
+                }
+            }
         }
 
-        return answer;
+        return (int) answer;
     }
-
-    int query(int n, long l) {
-        if (n == 0 || l == 0)
-            return 1;
-        if (l % 5 == 2)
-            return 0;
-        return query(n - 1, l / 5);
-    }
-
 }
