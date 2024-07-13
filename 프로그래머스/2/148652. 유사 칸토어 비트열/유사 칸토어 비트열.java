8930b965623c2886c47a14595574d3a097c1b00c
je-pa <76720692+je-pa@users.noreply.github.com>
2024-07-13 12:41:52 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 121.41 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
@@ -1,26 +1,21 @@
+
 class Solution {
     public int solution(int n, long l, long r) {
-        long ln = 0;
-        long rn = 0;
-        for(int i=0 ; i<= 20; i++){
-            if(Math.pow(5,i) <= l-1 && l-1 <= Math.pow(5,i+1)){
-                ln = i;
-            }
-            if(Math.pow(5,i) <= r && r <= Math.pow(5,i+1)){
-                rn = i;
-                break;
-            }
-            
+        int answer = 0;
+
+        for (long val = l; val <= r; val++) {
+            answer += query(n, val - 1);
         }
-        
-        return (int)(re(r,rn+1) - re(l-1,ln+1));
+
+        return answer;
     }
-    long re(long l, long n){
-        if(n<1) return 0;
-        if(l == 0) return 0;
-        long m =l/(long)Math.pow(5,n-1);
-        if(m==2) return m*(long)Math.pow(4,n-1);
-        if(m>2) m-=1;
-        return m*(long)Math.pow(4,n-1) + re(l%(long)Math.pow(5,n-1), n-1);
+
+    int query(int n, long l) {
+        if (n == 0 || l == 0)
+            return 1;
+        if (l % 5 == 2)
+            return 0;
+        return query(n - 1, l / 5);
     }
+
 }
