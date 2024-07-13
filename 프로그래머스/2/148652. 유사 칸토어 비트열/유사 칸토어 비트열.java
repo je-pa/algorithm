dbc2aa25f7e6b672683824c82b3ea6e02461f85d
je-pa <76720692+je-pa@users.noreply.github.com>
2024-07-13 12:09:20 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 16.09 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int n, long l, long r) {
+        long ln = 0;
+        long rn = 0;
+        for(int i=0 ; i<= 20; i++){
+            if(Math.pow(5,i) <= l-1 && l-1 <= Math.pow(5,i+1)){
+                ln = i;
+            }
+            if(Math.pow(5,i) <= r && r <= Math.pow(5,i+1)){
+                rn = i;
+                break;
+            }
+            
+        }
+        System.out.println(l +" "+ln);
+        System.out.println(re(l-1,ln+1));
+        System.out.println(r +" "+rn);
+        System.out.println(re(r,rn+1));
+        
+        return (int)(re(r,rn+1) - re(l-1,ln+1));
+    }
+    long re(long l, long n){
+        if(n<1) return 0;
+        if(l == 0) return 0;
+        // System.out.println(l + " "+ n);
+        long m =l/(long)Math.pow(5,n-1);
+        if(m==2) return m*(long)Math.pow(4,n-1);
+        if(m>2) m-=1;
+        return m*(long)Math.pow(4,n-1) + re(l%(long)Math.pow(5,n-1), n-1);
+    }
+}
