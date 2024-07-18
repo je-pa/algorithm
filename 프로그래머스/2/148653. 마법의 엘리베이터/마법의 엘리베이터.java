6a52e09f85f58c61fc3df64022f4d4ba8fe333ee
je-pa <76720692+je-pa@users.noreply.github.com>
2024-07-18 10:48:44 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int storey) {
+        return DFS(storey);
+    }
+        
+    // 16 -> 20 -> 0
+    public int DFS(int storey) {
+        if(storey == 0) {
+            return 0;
+        }
+                        
+        int value = storey%10;
+        int check = (storey%100)/10;
+              
+        // 0~4 일 때 n만큼 감소, 6~9일 때 10 - n 만큼 감소 
+        // 5일 땐 다음 값이 0~4면 +5, 5~9면 10 - n만큼 감소 
+        if(value <= 4 || (value == 5 && check <= 4)) {
+            return value + DFS(storey/10);
+        }else {
+            return 10 - value + DFS(storey/10 + 1);
+        }      
+    } 
+}
