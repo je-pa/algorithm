c92ebaa7d56e1ff12e75b520887cd476534c05b5
je-pa <76720692+je-pa@users.noreply.github.com>
2024-09-23 12:12:30 +0900

    [level 3] Title: 산 모양 타일링, Time: 8.66 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -1,24 +1,39 @@
+// class Solution {
+//     public int solution(int n, int[] tops) {
+//         int answer = 1;
+//         int minus = 0;
+//         int beforeAnswer = 0;
+//         for(int top : tops){
+//             beforeAnswer = answer;
+//             if(top == 1){
+//                 answer = (answer*4)%10007;
+//             }else{
+//                 answer = (answer*3)%10007;
+//             }
+//             if(minus > answer){
+//                 answer = answer + 10007 - minus;
+//             }else{
+//                 answer -= minus;
+//             }
+            
+//             minus = beforeAnswer;
+            
+//         }
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int solution(int n, int[] tops) {
-        int answer = 1;
-        int minus = 0;
-        int beforeAnswer = 0;
-        for(int top : tops){
-            beforeAnswer = answer;
-            if(top == 1){
-                answer = (answer*4)%10007;
+        int[] dp = new int[2*tops.length+3];
+        dp[1] = 1;
+        for(int i=2; i<dp.length; i++){
+            if(i % 2 == 0 || tops[i/2 - 1] == 0){
+                dp[i] = (dp[i-1] + dp[i-2]) % 10007;
             }else{
-                answer = (answer*3)%10007;
+                dp[i] = (2*dp[i-1] + dp[i-2]) % 10007;
             }
-            if(minus > answer){
-                answer = answer + 10007 - minus;
-            }else{
-                answer -= minus;
-            }
-            
-            minus = beforeAnswer;
-            
         }
-        return answer;
+        return dp[dp.length-1];
     }
 }
