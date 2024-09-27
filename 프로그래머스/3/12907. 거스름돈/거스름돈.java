e11d6a11113706784c062ef74c8ab00ba92418c3
je-pa <76720692+je-pa@users.noreply.github.com>
2024-09-28 01:43:51 +0900

    [level 3] Title: 거스름돈, Time: 19.47 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,21 +1,15 @@
 class Solution {
-      private int[][] dp;
 
-    public int solution(int n, int[] money) {
+    public int solution(int total, int[] coins) {
 
-        dp = new int[money.length + 1][n + 1];//이차원 배열 생성
-
-        for (int i = 1; i <= money.length; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (j == 0) {
-                    dp[i][j] = 1;//맨 첫 열은 1로 초기화
-                } else if (j - money[i-1] >= 0) {
-                    dp[i][j] = (dp[i - 1][j] + dp[i][j - money[i - 1]]) % 1000000007;
-                } else
-                    dp[i][j] = dp[i - 1][j];
+        int[] arr = new int[total];
+        for(int k = 0;k<total;k++){ arr[k] = 0; }
+        for(int i : coins){
+            arr[i-1] += 1;
+            for(int j=0; j<total ; j++) {
+                if(j>=i) { arr[j] += arr[j - i]; }
             }
         }
-
-        return dp[money.length][n];
+        return arr[total-1];
     }
 }
