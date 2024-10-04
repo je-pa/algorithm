a6fd86cdc25db3551f5085a0a16e04a4be448205
je-pa <76720692+je-pa@users.noreply.github.com>
2024-10-04 23:39:41 +0900

    [level 3] Title: 베스트앨범, Time: 1.53 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] genres, int[] plays) {
+        Map<String, Count> m = new HashMap<>();
+        
+        for(int i=0 ; i<genres.length ; i++){
+            m.put(genres[i], m.getOrDefault(genres[i], new Count()).plus(plays[i], i));
+        }
+        
+        // m.entrySet();
+        PriorityQueue<Count> p = new PriorityQueue<>();
+        for(String str : m.keySet()){
+            p.add(m.get(str));
+        }
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        while(!p.isEmpty()){
+            Count c = p.poll();
+            for(int i=0 ; !c.indexs.isEmpty() && i<2  ; i++){
+                list.add(c.indexs.poll().index);
+            }
+        }
+        int[] result = new int[list.size()];
+        for(int i = 0 ; i<list.size() ; i++){
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+
+    static class Count implements Comparable<Count>{
+        int plays;
+        PriorityQueue<G> indexs = new PriorityQueue<>();
+        
+        public Count plus(int plays, Integer index){
+            this.plays += plays;
+            this.indexs.add(new G(plays, index));
+            return this;
+        }
+        
+        public int compareTo(Count c){
+            return Integer.compare(c.plays, this.plays);
+        }
+    }
+    
+    static class G implements Comparable<G>{
+        int plays;
+        int index;
+        public G(int plays, int index){
+            this.plays = plays;
+            this.index = index;
+        }
+        public int compareTo(G g){
+            if(g.plays != this.plays) return Integer.compare(g.plays, this.plays);
+            return Integer.compare(this.index, g.index);
+        }
+    }
+}
