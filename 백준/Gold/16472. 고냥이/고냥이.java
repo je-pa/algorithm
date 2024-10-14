98243c25fd56bea5d1e3c0ad10d9b274b7dd1be9
je-pa <76720692+je-pa@users.noreply.github.com>
2024-10-14 15:44:20 +0900

    [Gold IV] Title: 고냥이, Time: 208 ms, Memory: 19500 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -7,16 +7,11 @@ public class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
     char[] arr = br.readLine().toCharArray();
-//    long result = 0;
     int l = 0;
-    int r = 0;
-//    int[] count = new int['z'-'a'+1];
+    int r = -1;
     int max = 0;
     Map<Character, Integer> map = new HashMap<>();
-    map.put(arr[r], 1);
-//    count[arr[r]]+=1;
     while(r != arr.length){
-      max = Math.max(max, r-l+1);
       r++;
       if(r == arr.length) break;
       map.put(arr[r], map.getOrDefault(arr[r],0)+1);
@@ -27,6 +22,7 @@ public class Main {
         }
         l++;
       }
+      max = Math.max(max, r-l+1);
 
     }
     System.out.println(max);
