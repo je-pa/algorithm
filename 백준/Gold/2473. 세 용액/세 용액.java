88b579f9998d02747fc0214e8f655f50ee9c05ef
je-pa <76720692+je-pa@users.noreply.github.com>
2024-10-14 20:29:36 +0900

    [Gold III] Title: 세 용액, Time: 332 ms, Memory: 16688 KB -BaekjoonHub

---
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    long min = Long.MAX_VALUE;
+    Arrays.sort(arr);
+    int[] result = new int[3];
+    for(int i = 0; i < n; i++) {
+      int l = 0;
+      int r = n-1;
+
+      while(l < r){
+        if(r == i) {
+          r--;
+          continue;
+        }
+        if(l == i){
+          l++;
+          continue;
+        }
+        long ai = arr[i];
+        long al = arr[l];
+        long ar = arr[r];
+        if(Math.abs(ai + al + ar) < Math.abs(min)){
+          result[0] = (int)ai;
+          result[1] = (int)al;
+          result[2] = (int)ar;
+          min = ai + al + ar;
+        }
+        if(ai + al +ar < 0){
+          l++;
+        }else if(ai + al +ar > 0){
+          r--;
+        }else{
+          break;
+        }
+      }
+    }
+    Arrays.sort(result);
+    System.out.printf("%d %d %d",result[0], result[1], result[2]);
+  }
+
+}
