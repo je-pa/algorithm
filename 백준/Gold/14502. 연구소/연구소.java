a7aa8006805155686aa962da23a4fa33999c8126
je-pa <76720692+je-pa@users.noreply.github.com>
2024-10-15 21:25:23 +0900

    [Gold IV] Title: 연구소, Time: 572 ms, Memory: 24200 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,77 +1,79 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-    static int n,m,result=0;
-    static int[][] map = new int[8][8];
-    static int[][] temp = new int[8][8];
+  public static Set<Integer> set = new TreeSet<>();
+  public static int n;
+  public static int m;
+  public static int[][] map;
+  public static boolean[][] visit;
+  public static int[] dx = {1, -1, 0, 0};
+  public static int[] dy = {0, 0, 1, -1};
+  public static List<int[]> list = new ArrayList<>();
+  public static int maxZero = 0;
 
-    static int[] dx = {-1, 1, 0, 0};//상 하 좌 우
-    static int[] dy = {0, 0, -1, 1};
-
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] nm = br.readLine().split(" ");
-        n = Integer.parseInt(nm[0]);
-        m = Integer.parseInt(nm[1]);
-
-        for(int i=0; i<n ; i++){
-            map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        }
-        dfs(0);
-        System.out.println(result);
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+    map = new int[n][m];
+    visit = new boolean[n][m];
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < m; j++) {
+        int num = Integer.parseInt(st.nextToken());
+        map[i][j] = num;
+        if(num == 2) list.add(new int[]{i, j});
+      }
     }
-
-    static void dfs(int count){
-        if(count == 3){
-            for(int i =0;i<n;i++){
-                for(int j=0 ; j<m ; j++){
-                    temp[i][j] = map[i][j];
-                }
-            }
-            for(int i =0;i<n;i++){
-                for(int j=0 ; j<m ; j++){
-                    if(temp[i][j]==2) virus(i,j);
-                }
-            }
-            result = Math.max(result, count());
-            return;
-        }
-        for(int i=0; i<n ; i++){
-            for(int j=0; j<m ; j++){
-                if(map[i][j] == 0){
-                    count++;
-                    map[i][j]=1;
-                    dfs(count);
-                    map[i][j]=0;
-                    count--;
-                }
-            }
-        }
+    setWall(0, 0, 0);
+    System.out.println(maxZero);
+  }
+  public static void setWall(int x, int y, int count){
+    if(x >= n || y >= m) return;
+    if(count == 3) {
+      setVisit();
+      for(int[] arr : list) {
+        virus(arr[0], arr[1]);
+      }
+      zeroCount();
+      return;
     }
-
-    static void virus(int x, int y){
-        for(int i=0; i<4 ; i++){
-            int nx = x+dx[i];
-            int ny = y+dy[i];
-            if(nx<0 || nx>=n || ny<0 || ny>=m){
-                continue;
-            }
-            if(temp[nx][ny]==0){
-                temp[nx][ny]=2;
-                virus(nx, ny);
-            }
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < m; j++) {
+        if(map[i][j] == 0){
+          map[i][j] = -1;
+          setWall(i, j, count+1);
+          map[i][j] = 0;
         }
+      }
     }
+  }
 
-    static int count(){
-        int count =0;
-        for(int i=0 ;i<n ; i++){
-            for(int j=0; j<m; j++){
-                if(temp[i][j]==0){
-                    count++;
-                }
-            }
-        }
-        return count;
+  private static void zeroCount() {
+    int zero = 0;
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < m; j++) {
+        if(map[i][j] == 0 && !visit[i][j]) zero++;
+      }
     }
+    maxZero = Math.max(maxZero, zero);
+  }
+
+  public static void setVisit(){
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < m; j++){
+        visit[i][j] = false;
+      }
+    }
+  }
+  public static void virus(int x, int y){
+    if(x < 0 || y < 0 || x >= n || y >= m) return;
+    if(visit[x][y] || map[x][y] == 1 || map[x][y] == -1) return;
+    visit[x][y] =true;
+    for(int i =0 ; i < dx.length ; i++){
+      virus(x+dx[i], y+dy[i]);
+    }
+  }
 }
