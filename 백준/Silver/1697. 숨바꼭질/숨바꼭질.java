ba93b751f8c75643216ac5f1076783cc908ceb2f
je-pa <76720692+je-pa@users.noreply.github.com>
2024-10-16 10:00:46 +0900

    [Silver I] Title: 숨바꼭질, Time: 200 ms, Memory: 31300 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -4,26 +4,15 @@ import java.io.*;
 public class Main {
   public static int n;
   public static int m;
-//  public static int[][] map;
   public static boolean[] visit;
   public static int[] dx = {1, -1, 0};
-//  public static int[] dy = {0, 1, 0, -1};
-//  public static int min = 10001;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
     n = Integer.parseInt(st.nextToken());
     m = Integer.parseInt(st.nextToken());
-//    map = new int[n][m];
     visit = new boolean[200_001];
-//    for (int i = 0; i < n; i++) {
-//      String s = br.readLine();
-//      for (int j = 0; j < m; j++) {
-//        int num = s.charAt(j) - '0';
-//        map[i][j] = num;
-//      }
-//    }
 
     System.out.println(bfs());
   }
@@ -37,12 +26,7 @@ public class Main {
       int curX = q.poll();
       int count = q.poll();
       for(int i = 0; i < 3; i++){
-        int nx = 0;
-        if(dx[i] == 0){
-          nx= curX * 2;
-        }else{
-          nx= curX + dx[i];
-        }
+        int nx = curX + (dx[i] != 0 ? dx[i] : curX);
         int nc = count + 1;
         if(nx <0 || nx >= 200_001) continue;
         if(visit[nx]) continue;
