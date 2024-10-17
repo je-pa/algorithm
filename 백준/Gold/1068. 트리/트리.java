5ce97e64e906d5616e1deae89d7aa26df88fece8
je-pa <76720692+je-pa@users.noreply.github.com>
2024-10-17 09:31:43 +0900

    [Gold V] Title: 트리, Time: 100 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,47 +1,50 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-
+import java.util.*;
+import java.io.*;
 public class Main {
-    static int N = 0;
-    static int[] arr;
-    static int REMOVE = 0;
-    static int ROOT = 0;
-    static int count = 0;
-    static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-    public static void main(String[] args) throws Exception{
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        REMOVE = Integer.parseInt(br.readLine());
+  public static int n;
+  public static int root;
 
-        for(int i=0 ; i<N ; i++){
-            list.add(new ArrayList<>());
-        }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    n = Integer.parseInt(br.readLine());
+    List<List<Integer>> list = new ArrayList<>();
+    for(int i = 0; i < n; i++) {
+      list.add(new ArrayList<>());
+    }
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for(int i = 0; i < n; i++) {
+      int parent = Integer.parseInt(st.nextToken());
+      if(parent == -1) {
+        root = i;
+        continue;
+      }
+      list.get(parent).add(i);
+    }
+    int remove = Integer.parseInt(br.readLine());
+    if(remove == root) {
+      System.out.println(0);
+      return;
+    }
+    int count = 0;
+    LinkedList<Integer> q = new LinkedList<>();
+    q.add(root);
+    while(!q.isEmpty()) {
+      int cur = q.poll();
+      List<Integer> childs = list.get(cur);
+      if(childs.size() == 0) {
+        count++;
+        continue;
+      }else if(childs.size() == 1 && childs.get(0) == remove) {
+        count++;
+        continue;
+      }
+      for(Integer child : childs) {
+        if(child == remove) continue;
+        q.add(child);
+      }
+    }
+    System.out.println(count);
+  }
 
-        for(int i=0 ; i<N ; i++){
-            if(arr[i]==-1){
-                ROOT = i;
-            }
-            if(i==REMOVE) continue;
-            if(arr[i]==-1) continue;
-            list.get(arr[i]).add(i);
-        }
-        if(ROOT == REMOVE){
-            System.out.println(0);
-            return;
-        }
-        dfs(ROOT);
-        System.out.println(count);
-    }
-    static void dfs(int index){
-        if(list.get(index).isEmpty()) {
-            count++;
-            return;
-        }
-        for(int i : list.get(index)){
-            dfs(i);
-        }
-    }
+
 }
