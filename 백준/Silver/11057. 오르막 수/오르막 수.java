c7e47305dafda8f4b15b92f2c399f8cbdda8a108
je-pa <76720692+je-pa@users.noreply.github.com>
2024-10-22 10:05:43 +0900

    [Silver I] Title: 오르막 수, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -1,29 +1,30 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
 
-        int[][] arr = new int[n+1][10];
-
-        for(int i=0 ; i<10 ; i++){
-            arr[1][i] = 1;
-        }
-        for(int i=1 ; i<n+1 ; i++){
-            arr[i][0] = 1;
-        }
-        for(int i=2 ; i<n+1 ; i++){
-            for(int j=1; j<10 ; j++){
-                arr[i][j] = (arr[i-1][j] + arr[i][j-1])%10_007;
-            }
-        }
-        int sum = 1;
-        for(int i=1; i<10 ; i++){
-            sum += arr[n][i];
-            sum %= 10_007;
-        }
-        System.out.println(sum);
+    int[][] dp = new int[n+1][10];
+    for(int i = 1; i < n+1; i++){
+      dp[i][0] = 1;
     }
+    for(int i=1 ;i<10; i++){
+      dp[1][i] = 1;
+    }
+
+    for (int i = 2; i < n+1; i++) {
+      for (int j = 1; j < 10; j++) {
+        dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        dp[i][j] %= 10_007;
+      }
+    }
+    int sum =0;
+    for(int i=0 ;i<10 ;i++){
+      sum+=dp[n][i];
+      sum%=10_007;
+    }
+
+    System.out.println(sum);
+  }
 }
