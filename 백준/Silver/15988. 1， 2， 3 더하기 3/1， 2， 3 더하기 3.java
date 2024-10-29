930cab6ca066e8008d4f085d2d5c2dbfa9d9024d
je-pa <76720692+je-pa@users.noreply.github.com>
2024-10-30 08:56:38 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 132 ms, Memory: 18160 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -1,19 +1,38 @@
 import java.io.*;
 
-public class Main{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-        int n = stoi(br.readLine());
-        long dp[] = new long[1_000_001];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-        for(int i = 4; i <= 1_000_000; i++)
-        	dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % 1_000_000_009;
-        for(int i = 0; i < n; i++){
-        	System.out.println(dp[stoi(br.readLine())]);
-        }
+public class Main {
+  public static int N;
+  public static int MOD = 1_000_000_009;
+  public static int[] dp;
+  public static int max = 0;
+  public static int[] arr;
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(br.readLine());
+    arr = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      int k = Integer.parseInt(br.readLine());
+      max = Math.max(max, k);
+      arr[i] = k;
     }
-	public static int stoi(String str) {return Integer.parseInt(str);}
+
+    dp = new int[max+1];
+    pro();
+    StringBuilder sb = new StringBuilder();
+    for(int i = 0; i < N; i++) {
+      sb.append(dp[arr[i]]).append("\n");
+    }
+    System.out.println(sb);
+  }
+
+  public static void pro(){
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4; // 7 13 24 44 81 149 274
+    for(int i = 4; i <= max; i++) {
+      dp[i] = ((dp[i-1] + dp[i-2]) % MOD + dp[i-3]) % MOD;
+    }
+  }
 }
