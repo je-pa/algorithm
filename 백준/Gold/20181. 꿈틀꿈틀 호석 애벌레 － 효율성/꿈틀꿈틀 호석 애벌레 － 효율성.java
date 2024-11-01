a2020f5054ef126fd53fb6cc87d8d58d00e35219
je-pa <76720692+je-pa@users.noreply.github.com>
2024-11-01 13:22:39 +0900

    [Gold II] Title: 꿈틀꿈틀 호석 애벌레 - 효율성, Time: 316 ms, Memory: 43852 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  public static int N;
+  public static int K;
+  public static int[] arr;
+  public static int[] dp;
+  public static ArrayList<ArrayList<int[]>> deps = new ArrayList<>();
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    input(br);
+    pro();
+    System.out.println(dp[N]);
+  }
+  public static void input(BufferedReader br) throws Exception{
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
+    arr = new int[N];
+    dp = new int[N+1];
+    for(int i = 0 ; i<=N ; i++){
+      deps.add(new ArrayList<>());
+    }
+    st = new StringTokenizer(br.readLine());
+    for(int i=0 ; i<N ; i++){
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+  }
+
+  public static void pro(){
+    int j =0;
+    int sum = 0;
+    for(int i=0 ; i< N; i++){
+      while(j < N && sum < K){
+        sum += arr[j];
+        j++;
+      }
+      if(sum >= K){
+        deps.get(j).add(new int[]{i, sum-K});
+      }
+      sum -= arr[i];
+    }
+
+    for(int i=1 ; i<=N ; i++){
+      int curMax = 0;
+      for(int[] dep : deps.get(i)){
+        if(dep[0]==0){
+          curMax = Math.max(curMax, dep[1]);
+        }else{
+          curMax = Math.max(curMax, dp[dep[0]]+dep[1]);
+        }
+      }
+      dp[i] =  Math.max(dp[i-1], curMax);
+    }
+
+  }
+}
