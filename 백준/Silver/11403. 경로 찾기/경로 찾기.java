c8a73952a8428b66a1bc09877162c66d05654624
je-pa <76720692+je-pa@users.noreply.github.com>
2024-11-09 00:18:09 +0900

    [Silver I] Title: 경로 찾기, Time: 152 ms, Memory: 15672 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,54 +1,57 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
- 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
- 
-        int N = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N][N];
- 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        
-        // i에서 j까지 갈 수 있는가?
-        // i에서 k로 가고, k에서 j로 갈 수 있는가?
-        // 위에 2개의 질문은 동일함.
-        for (int k = 0; k < N; k++) {
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    // 단순히 갈 수 있는 경로가 있는지만 체크함.
-                    if (arr[i][k] == 1 && arr[k][j] == 1) {
-                        arr[i][j] = 1;
-                    }
-                }
-            }
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                sb.append(arr[i][j] + " ");
-            }
-            sb.append("\n");
-        }
-        
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+
+  public static int N;
+  public static ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
+  public static int[][] map;
+  public static boolean[] visited;
+  public static int result = Integer.MAX_VALUE;
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    input(br);
+    pro();
+    StringBuilder sb = new StringBuilder();
+    for(int i =0 ; i<N ; i++){
+      for(int j =0 ; j<N ; j++){
+        sb.append(map[i][j]).append(" ");
+      }
+      sb.append("\n");
     }
- 
+    System.out.println(sb);
+  }
+
+  static void input(BufferedReader br) throws Exception {
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    map = new int[N][N];
+    for (int i = 0; i < N; i++) {
+      lists.add(new ArrayList<>());
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < N; j++) {
+        int k = Integer.parseInt(st.nextToken());
+        if(k == 0) continue;
+        lists.get(i).add(j);
+      }
+    }
+  }
+  static void pro(){
+    for(int i = 0; i < N; i++){
+      visited = new boolean[N];
+      dfs(i, i, 0);
+    }
+  }
+  static void dfs(int root, int cur, int count){
+    if(visited[cur]) return;
+    if(count != 0) {
+      visited[cur] = true;
+      map[root][cur] = 1;
+    }
+    List<Integer> list = lists.get(cur);
+    for(int i = 0; i < list.size(); i++){
+      dfs(root, list.get(i), count + 1);
+    }
+  }
 }
- 
