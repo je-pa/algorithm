5f556146897f49766462d9cdf14f85d787b824f5
je-pa <76720692+je-pa@users.noreply.github.com>
2024-11-09 10:13:42 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 256 ms, Memory: 20748 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
@@ -1,69 +1,92 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
+    static int[][] floyd;
+    static int N, M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st1.nextToken());
+        M = Integer.parseInt(st1.nextToken());
 
-  public static int N;
-  public static int M;
-  public static int result = Integer.MAX_VALUE;
-  public static int min, max;
-  public static int[][] arr;
-
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    input(br);
-    pro();
-    StringBuilder sb = new StringBuilder();
-    sb.append(min);
-    sb.append(" ");
-    sb.append(max);
-    sb.append(" ");
-    sb.append(result*2);
-
-    System.out.println(sb);
-  }
-
-  static void input(BufferedReader br) throws Exception {
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    M = Integer.parseInt(st.nextToken());
-    arr = new int[N+1][N+1];
-    for(int i = 1; i <= N; i++) {
-      for(int j = 1; j <= N; j++) {
-        if(i == j) continue;
-        arr[i][j] = 101;
-      }
-    }
-    for (int i = 0; i < M; i++) {
-      st = new StringTokenizer(br.readLine());
-      int a = Integer.parseInt(st.nextToken());
-      int b = Integer.parseInt(st.nextToken());
-      arr[a][b] = 1;
-      arr[b][a] = 1;
-    }
-  }
-  static void pro(){
-    for(int i = 1; i <= N; i++) {
-      for(int j = 1; j <= N; j++) {
-        for(int k = 1; k <= N; k++) {
-          if(i== k || j == k) continue;
-          arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
-          arr[j][i] = Math.min(arr[j][i], arr[j][k] + arr[k][i]);
+        floyd = new int[N][N];
+        
+        for(int i = 0 ; i < N ; i++) {
+            for(int j = 0 ; j < N ; j++) {
+                floyd[i][j] = 101; //점의 최대 개수는 100개이므로, 점과 점은 아무리 멀어도 거리 99가 최대임.
+                //즉, 거리 101로 해주는 것은 다익스트라의 Integer.MAX_VALUE와 같음.
+            }
         }
-      }
+        //존재하는 길이 없다. 를 101 로 표현.
+        //있으면, 거리는 전부 1이니 해당 값 넣기
+        
+        for(int i = 0 ; i < M ; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st2.nextToken());
+            int b = Integer.parseInt(st2.nextToken());
+            floyd[a-1][b-1] = 1;
+            floyd[b-1][a-1] = 1;
+        }
+        //초깃값.
+
+        for(int i = 0 ; i < N ; i++) {
+            for(int j = 0 ; j < N ; j++) {
+                if(i == j) continue;
+                for(int k = 0 ; k < N ; k++) {
+                    if(i == k || j == k) continue;
+                    
+                    if(floyd[j][k] > floyd[j][i] + floyd[i][k]) floyd[j][k] = floyd[j][i] + floyd[i][k];
+                    
+                }
+            }
+        }
+        
+        //플로이드 워셜로 각 점에서 각 점까지의 거리의 최솟값 다 채운 후
+        //플로이드 워셜 로직 = a->c로 가는 값보다 b를 거쳐 a->b + b->c 값이 더 작다면 작은 값을 택하는 알고리즘.
+        
+        int min = Integer.MAX_VALUE;
+        int n1 = 0;
+        int n2 = 0;
+
+        for(int i = 0 ; i < N ; i++) {
+            for(int j = 0 ; j < N ; j++) {
+                if(i == j) continue;
+                //두 점을 고른다. (1, 2부터 N-1, N까지)
+
+                int now = sum(i, j);
+                //고른 두 점을 기준으로 각각의 점의 거리의 최솟값을 더해나간다.
+                //1, 2를 골랐으면
+                //sum(1, 2) = 그 둘(1, 2)에서 3까지의 거리의 최솟값 + ... + 그 둘(1, 2)에서 N 까지의 거리의 최솟값
+
+                if(min > now) {
+                    n1 = i+1;
+                    n2 = j+1;
+                    min = now;
+                }
+                //해당 값이 최솟값이 된다면 두 점을 기록해놓는다.
+
+            }
+        }
+
+        //기록한 두 점과 최솟값을 출력한다.
+        bw.write(String.valueOf(n1) + " " + String.valueOf(n2) + " " + String.valueOf(min*2)); //min*2는 왕복이라서.
+        bw.flush();
+        bw.close();
+
+
     }
-    for(int i = 1; i <= N; i++) {
-      for(int j = i+1; j <= N; j++) {
+
+    static int sum(int n1, int n2) {
         int sum = 0;
-        for(int k = 1; k <= N; k++) {
-          sum+=Math.min(arr[i][k], arr[j][k]);
+        for(int i = 0 ; i < N ; i++) {
+            if(i == n1 || i == n2) continue;
+            sum += Math.min(floyd[n1][i], floyd[n2][i]);
         }
-        if(sum < result){
-          result = sum;
-          min = i;
-          max = j;
-        }
-      }
+
+        return sum;
     }
-  }
+
+
 }
