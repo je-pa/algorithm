cc80305b6224a2e8120cf7649be4298ddd6a299e
je-pa <76720692+je-pa@users.noreply.github.com>
2024-11-09 09:52:52 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 180 ms, Memory: 16604 KB, Score: 11 point -BaekjoonHub

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
@@ -46,7 +46,7 @@ public class Main {
   }
   static void pro(){
     for(int i = 1; i <= N; i++) {
-      for(int j = 1; j < i; j++) {
+      for(int j = 1; j <= N; j++) {
         for(int k = 1; k <= N; k++) {
           if(arr[i][k] == Integer.MAX_VALUE || arr[k][j] == Integer.MAX_VALUE) continue;
           arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
@@ -55,18 +55,14 @@ public class Main {
       }
     }
     for(int i = 1; i <= N; i++) {
-      int sum = 0;
       for(int j = i+1; j <= N; j++) {
+        int sum = 0;
         for(int k = 1; k <= N; k++) {
           if(arr[i][k] == Integer.MAX_VALUE || arr[j][k] == Integer.MAX_VALUE) continue;
           sum+=Math.min(arr[i][k], arr[j][k]);
         }
         if(sum < result){
           result = sum;
-          min = i;
-          max = j;
-        }
-        if(sum == result && min >= i && max <= j){
           min = i;
           max = j;
         }
