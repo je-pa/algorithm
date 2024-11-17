01ffe7a0d8966b285573421b11622ab6148175c9
je-pa <76720692+je-pa@users.noreply.github.com>
2024-11-17 10:16:15 +0900

    [Silver I] Title: 1, 2, 3 더하기 5, Time: 140 ms, Memory: 19388 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
@@ -1,37 +1,52 @@
-
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static final int MOD = 1000000009;
-	static long[][] dp = new long[100001][4];
+  static int N;
+  static int MOD = 1_000_000_009;
+  static int[] arr;
 
-	public static void main(String[] args) throws Exception {
+  static StringBuilder result = new StringBuilder();
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    input(br);
+    pro();
+    System.out.println(result);
+  }
 
-		dp[1][1] = 1;
-		dp[2][2] = 1;
-		dp[3][1] = 1;
-		dp[3][2] = 1;
-		dp[3][3] = 1;
+  static void input(BufferedReader br) throws Exception {
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    arr = new int[N];
+    for(int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(br.readLine());
+    }
+  }
 
-		for (int i = 4; i <= 100000; i++) {
-			dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % MOD;
-			dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % MOD;
-			dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % MOD;
-		}
-
-		int T = Integer.parseInt(br.readLine());
-
-		for (int t = 0; t < T; t++) {
-			int n = Integer.parseInt(br.readLine());
-			long res = (dp[n][1] + dp[n][2] + dp[n][3]) % MOD;
-			bw.write(res + "\n");
-		}
-
-		bw.flush();
-	}
-
+  static void pro() {
+    int[][] dp = new int[100_001][4];
+    dp[1][1] = 1;
+    dp[2][2] = 1;
+    dp[3][1] = 1;
+    dp[3][2] = 1;
+    dp[3][3] = 1;
+    for(int i = 4; i < 100_001; i++) {
+      dp[i][1] = dp[i-1][2] + dp[i-1][3];
+      dp[i][1] %= MOD;
+      dp[i][2] = dp[i-2][1] + dp[i-2][3];
+      dp[i][2] %= MOD;
+      dp[i][3] = dp[i-3][1] + dp[i-3][2];
+      dp[i][3] %= MOD;
+    }
+    for(int i : arr){
+      int sum = 0;
+      for(int j : dp[i]){
+        sum += j;
+        sum %= MOD;
+      }
+      result.append(sum).append("\n");
+    }
+  }
 }
