5536dab7515a40327fc3942883d15aab0025751a
je-pa <76720692+je-pa@users.noreply.github.com>
2024-11-23 09:34:53 +0900

    [Gold V] Title: 1학년, Time: 108 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.java
+++ 백준/Gold/5557. 1학년/1학년.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
 
   static int N;
-  static long[] arr;
+  static int[] arr;
   static long[][] dp;
 
   public static void main(String[] args) throws Exception {
@@ -17,7 +17,7 @@ public class Main {
   static void input(BufferedReader br) throws Exception {
     StringTokenizer st = new StringTokenizer(br.readLine());
     N = Integer.parseInt(st.nextToken());
-    arr = new long[N];
+    arr = new int[N];
     dp = new long[N][21];
     st = new StringTokenizer(br.readLine());
     for (int j = 0; j < N; j++) {
@@ -26,14 +26,13 @@ public class Main {
   }
 
   static void pro() {
-    dp[0][(int)arr[0]] = 1;
+    dp[0][arr[0]] = 1;
     for (int i = 1; i < N-1; i++) {
-      for(long j=0 ; j<21 ; j++){
-        long before = dp[i-1][(int)j];
-        if(before==0) continue;
-        for(long k : new long[]{j+arr[i], j-arr[i]}){
+      for(int j=0 ; j<21 ; j++){
+        if(dp[i-1][j]==0) continue;
+        for(int k : new int[]{j+arr[i], j-arr[i]}){
           if(k<0 || k>20) continue;
-          dp[i][(int)k]+=before;
+          dp[i][k]+=dp[i-1][j];
         }
       }
     }
