26de666b502f9e2ad7b88333c366dada41f86ad6
je-pa <76720692+je-pa@users.noreply.github.com>
2024-11-25 10:06:52 +0900

    [level 3] Title: 스티커 모으기(2), Time: 17.29 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -11,8 +11,7 @@ class Solution {
         }
         if(len < 3) return Math.max(sticker[0], sticker[1]);
         int[][] dp2 = new int[len][2];
-        dp2[2][1] = sticker[2];
-        for(int i=3 ; i<len ; i++){
+        for(int i=2 ; i<len ; i++){
             dp2[i][0] = Math.max(dp2[i-1][0], dp2[i-1][1]);
             dp2[i][1] = dp2[i-1][0] + sticker[i];
         }
