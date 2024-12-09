741c2599c9687b3ab2ef9ec23fa75c227e129148
je-pa <76720692+je-pa@users.noreply.github.com>
2024-12-09 10:40:07 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 316 ms, Memory: 19608 KB -BaekjoonHub

---
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static int result;
+  static int N;
+  static int[][] arr;
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    input(br);
+    pro();
+    System.out.println(result);
+  }
+
+  static void input(BufferedReader br) throws Exception {
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    arr = new int[N][2];
+    for(int i=0 ; i<N ; i++){
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      arr[i][0] = a;
+      arr[i][1] = b;
+    }
+  }
+
+  static void pro() {
+    dfs(0);
+  }
+
+  static void dfs(int idx){
+    while(idx < N && arr[idx][0] <= 0) idx++;
+    if(idx == N) {
+      int count =0;
+      for(int i=0 ; i<N ; i++){
+        if(arr[i][0] <= 0){
+          count++;
+        }
+      }
+      result = Math.max(result, count);
+      return;
+    }
+    for(int i=0 ; i<N ; i++){
+      if(idx == i) continue;
+      if(arr[i][0] <= 0) continue;
+      arr[idx][0] -= arr[i][1];
+      arr[i][0] -= arr[idx][1];
+      dfs(idx+1);
+      arr[idx][0] += arr[i][1];
+      arr[i][0] += arr[idx][1];
+    }
+    int count =0;
+    for(int i=0 ; i<N ; i++){
+      if(arr[i][0] <= 0){
+        count++;
+      }
+    }
+    result = Math.max(result, count);
+  }
+
+
+}
+
+// 16 21
+// 15 10.5
+// 14 5.25
+// 13 2.125
+// 12 1.0 -> 8 ^ 4
+//
