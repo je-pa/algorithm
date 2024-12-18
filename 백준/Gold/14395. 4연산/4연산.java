ac1d8dfa0399c0f4c6450db908c75627c9275b8e
je-pa <76720692+je-pa@users.noreply.github.com>
2024-12-18 11:24:26 +0900

    [Gold IV] Title: 4연산, Time: 108 ms, Memory: 14596 KB -BaekjoonHub

--- 백준/Gold/14395. 4연산/4연산.java
+++ 백준/Gold/14395. 4연산/4연산.java
@@ -5,7 +5,7 @@ public class Main {
   static String result = "-1";
   static long N;
   static long M;
-  static char[] cs = {'*', '+', '-', '/'};
+  static char[] cs = {'*', '+', '/'};
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     input(br);
@@ -33,11 +33,10 @@ public class Main {
     q.add(new StringBuilder().append(N));
     set.add(N);
     q.add(new StringBuilder());
-    PriorityQueue<String> pq = new PriorityQueue<>();
     while(!q.isEmpty()) {
       long cn = Long.parseLong(q.poll().toString());
       StringBuilder cc = q.poll();
-      for(int i=0 ; i<4 ; i++){
+      for(int i=0 ; i<3 ; i++){
         long nn = calc(cn, cn, cs[i]);
         StringBuilder nc = new StringBuilder(cc).append(cs[i]);
         if(nn == M){
@@ -45,14 +44,11 @@ public class Main {
           return;
         }
         if(set.contains(nn)) continue;
-        if(nn < 0 || nn > 1_000_000_000) continue;
+        if(nn == 0 || nn > M) continue;
         q.add(new StringBuilder().append(nn));
         q.add(nc);
         set.add(nn);
       }
-    }
-    if(!pq.isEmpty()) {
-      result = pq.poll();
     }
   }
 
