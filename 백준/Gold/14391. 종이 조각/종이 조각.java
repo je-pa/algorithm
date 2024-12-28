e8bb85cf36c071914b004a9b9efa350c04a6ecda
je-pa <76720692+je-pa@users.noreply.github.com>
2024-12-28 12:25:07 +0900

    [Gold III] Title: 종이 조각, Time: 152 ms, Memory: 19332 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.java
+++ 백준/Gold/14391. 종이 조각/종이 조각.java
@@ -1,62 +1,80 @@
-import java.util.*;
 import java.io.*;
- 
+import java.util.*;
+
 public class Main {
-    static int N,M;
-    static int paper[][];
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        paper = new int[N][M];
-        
-        for(int i=0; i<N; i++) {
-            String str = br.readLine();
-            for(int j=0; j<M; j++) {
-                paper[i][j] = str.charAt(j) - '0';
-            }
-        }
-        
-        int ans = 0;
-        
-        for(int s=0; s<(1<<(N*M)); s++) {
-            int sum = 0;
-            /* 가로(0) 찾기 */
-            for(int i=0; i<N; i++) {
-                int cur = 0;
-                for(int j=0; j<M; j++) {
-                    int k = i*M+j;    
-                    if( (s&(1<<k)) ==0 ) {    // s의 k번째 비트가 0이면-> 해당 숫자는 가로
-                        cur*= 10;    
-                        cur += paper[i][j];    
-                    }else {    // 해당 숫자는 세로
-                        sum += cur;
-                        cur = 0;
-                    }
-                }
-                sum += cur;
-            }
-            
-            /* 세로(1) 찾기 */
-            for(int j=0; j<M; j++) {
-                int cur = 0;
-                for(int i=0; i<N; i++) {
-                    int k = i*M +j;
-                    if( (s&(1<<k)) != 0) {    // s의 k번째 비트가 1이면-> 해당 숫자는 세로
-                        cur *= 10;
-                        cur += paper[i][j];
-                    }else {    // 해당 숫자는 가로
-                        sum += cur;
-                        cur = 0;
-                    }
-                }
-                sum += cur;
-            }
-            ans = Math.max(ans, sum);
-        }
-        
-        System.out.println(ans);        
+  static int result;
+  static int N;
+  static int M;
+  static int[][] arr;
+  static boolean[][] check;
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    input(br);
+    pro();
+    System.out.println(result);
+  }
+
+  static void input(BufferedReader br) throws Exception {
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    arr = new int[N][M];
+    check = new boolean[N][M];
+    for (int i = 0; i < N; i++) {
+      String line = br.readLine();
+      for(int j = 0; j < M; j++) {
+        int a = line.charAt(j)-'0';
+        arr[i][j] = a;
+      }
     }
- 
+  }
+
+  static void pro() {
+    dfs(0,0);
+  }
+  static void dfs(int x, int y){
+    if(y==M){
+      dfs(x+1, 0);
+      return;
+    }
+    if(x==N){
+      sum();
+      return;
+    }
+    check[x][y] = true;
+    dfs(x, y+1);
+
+    check[x][y] = false;
+    dfs(x, y+1);
+  }
+
+  static void sum(){
+    int sum = 0;
+    for(int i=0; i<N; i++){
+      int cur = 0;
+      for(int j=0; j<M; j++){
+        if(check[i][j]){
+          cur = cur * 10 + arr[i][j];
+        }else{
+          sum += cur;
+          cur = 0;
+        }
+      }
+      sum+=cur;
+    }
+    for(int i=0; i<M; i++){
+      int cur = 0;
+      for(int j=0; j<N; j++){
+        if(!check[j][i]){
+          cur = cur*10 + arr[j][i];
+        }else{
+          sum += cur;
+          cur = 0;
+        }
+      }
+      sum += cur;
+    }
+    result = Math.max(sum, result);
+  }
 }
