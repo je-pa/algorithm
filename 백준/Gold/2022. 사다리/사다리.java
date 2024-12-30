ece3f989824c608e5c0afffe069738bdd669abd7
je-pa <76720692+je-pa@users.noreply.github.com>
2024-12-30 10:56:39 +0900

    [Gold IV] Title: 사다리, Time: 108 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Gold/2022. 사다리/사다리.java
+++ 백준/Gold/2022. 사다리/사다리.java
@@ -11,15 +11,15 @@ public class Main {
 
         double left = 0, right = Math.min(x, y);
 
-        while (right > left + 0.001) {
+        while (right > left) {
             double width = (left + right) / 2;
             double h1 = Math.sqrt(Math.pow(x, 2) - Math.pow(width, 2));
             double h2 = Math.sqrt(Math.pow(y, 2) - Math.pow(width, 2));
             double res = (h1 * h2) / (h1 + h2);
   
-            if (res >= c) left = width;
-            else right = width;
+            if (res >= c) left = width + 0.001;
+            else right = width - 0.001;
         }
-        System.out.println(String.format("%.3f", right));
+        System.out.println(String.format("%.3f", left));
     }
 }
