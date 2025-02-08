1f0a27267a771f37d03e723be79b6086fea72a6a
je-pa <76720692+je-pa@users.noreply.github.com>
2025-02-08 23:49:17 +0900

    [Silver I] Title: 꿈틀꿈틀 호석 애벌레 - 기능성, Time: 104 ms, Memory: 14304 KB, Score: 39 point -BaekjoonHub

--- 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.java
+++ 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.java
@@ -3,59 +3,57 @@ import java.io.*;
 
 public class Main {
   public static int N;
-  public static int K;
-  public static int result = 0;
-  public static int[] foods;
-  public static int[] arr;
-  public static int[] countArr;
+  public static long K;
+  public static long[] arr;
+  public static long[] dp;
+  public static ArrayList<ArrayList<long[]>> deps = new ArrayList<>();
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     input(br);
     pro();
-    System.out.println(result);
+    System.out.println(dp[N]);
   }
   public static void input(BufferedReader br) throws Exception{
     StringTokenizer st = new StringTokenizer(br.readLine());
     N = Integer.parseInt(st.nextToken());
     K = Integer.parseInt(st.nextToken());
-    foods = new int[N];
-    arr = new int[N];
-    countArr = new int[N];
+    arr = new long[N];
+    dp = new long[N+1];
+    for(int i = 0 ; i<=N ; i++){
+      deps.add(new ArrayList<>());
+    }
     st = new StringTokenizer(br.readLine());
-    for(int i = 0; i < N; i++){
-      int k = Integer.parseInt(st.nextToken());
-      foods[i] = k;
+    for(int i=0 ; i<N ; i++){
+      arr[i] = Integer.parseInt(st.nextToken());
     }
   }
 
   public static void pro(){
-    int i=0;
-    int j=0;
-    int sum = 0;
-    int lastJ = -1;
-    int lastSum = 0;
-    while(i < N && j <= N){
-      if(sum < K){
-        if(j == N) break;
-        sum += foods[j];
+    int j =0;
+    long sum = 0;
+    for(int i=0 ; i< N; i++){
+      while(j < N && sum < K){
+        sum += arr[j];
         j++;
-      }else{
-        if(i<=lastJ && lastJ < j){
-          if(sum > lastSum){
-            result += (sum-K);
-            result -= (lastSum-K);
-            lastJ = j-1;
-            lastSum = sum;
-          }
-        }else{
-          result += sum -K;
-          lastJ = j-1;
-          lastSum = sum;
-        }
-        sum -= foods[i];
-        i++;
       }
+      if(sum >= K){
+        deps.get(j).add(new long[]{i, sum-K});
+      }
+      sum -= arr[i];
     }
+
+    for(int i=1 ; i<=N ; i++){
+      long curMax = 0;
+      for(long[] dep : deps.get(i)){
+        if(dep[0]==0){
+          curMax = Math.max(curMax, dep[1]);
+        }else{
+          curMax = Math.max(curMax, dp[(int)dep[0]]+dep[1]);
+        }
+      }
+      dp[i] =  Math.max(dp[i-1], curMax);
+    }
+
   }
 }
