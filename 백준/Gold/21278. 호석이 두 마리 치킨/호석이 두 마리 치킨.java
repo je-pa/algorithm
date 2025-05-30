2cf565602788ed54c94b891d7efe8ca5c2a66177
je-pa <76720692+je-pa@users.noreply.github.com>
2025-05-30 11:57:40 +0900

    [Gold IV] Title: 호석이 두 마리 치킨, Time: 164 ms, Memory: 15660 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
@@ -46,6 +46,7 @@ public class Main {
     for(int i = 1; i <= N; i++) {
       for(int j = 1; j <= N; j++) {
         for(int k = 1; k <= N; k++) {
+          if(arr[j][i] == 101 || arr[i][k] == 101) continue;
           arr[j][k] = Math.min(arr[j][k], arr[j][i] + arr[i][k]);
         }
       }
