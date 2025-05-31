f83ff7d0f874a48fd9b56bfca57b07c631be186a
je-pa <76720692+je-pa@users.noreply.github.com>
2025-05-31 10:40:46 +0900

    [Gold V] Title: 노드사이의 거리, Time: 1100 ms, Memory: 18788 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -1,64 +1,62 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 public class Main {
+  static int N, M;
+  static int[][] map;
+  static int[][] arr;
 
-  public static int N;
-  public static int M;
-  public static int result = Integer.MAX_VALUE;
-  public static int min, max;
-  public static int[][] arr;
-  public static int[][] twos;
-
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     input(br);
     pro();
-    StringBuilder sb = new StringBuilder();
-    for(int[] two : twos){
-      sb.append(arr[two[0]][two[1]]).append('\n');
+  }
+
+  static void pro() {
+    for(int k = 1 ; k <= N ; k++) {
+      for(int i = 1 ; i < N ; i++){
+        for(int j = i+1 ; j <= N ; j++){
+          if(map[i][k] == Integer.MAX_VALUE) continue;
+          if(map[j][k] == Integer.MAX_VALUE) continue;
+          map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+          map[j][i] = map[i][j];
+        }
+      }
+    }
+    StringBuilder sb= new StringBuilder();
+    for(int[] a : arr){
+      sb.append(map[a[0]][a[1]]).append("\n");
     }
     System.out.println(sb);
   }
 
-  static void input(BufferedReader br) throws Exception {
+  static void input(BufferedReader br) throws IOException {
     StringTokenizer st = new StringTokenizer(br.readLine());
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
-    arr = new int[N+1][N+1];
-    twos = new int[M][2];
-    for(int i = 1; i <= N; i++) {
-      for(int j = 1; j <= N; j++) {
-        if(i == j) continue;
-        arr[i][j] = Integer.MAX_VALUE;
+    map = new int[N+1][N+1];
+    arr = new int[M][2];
+    for(int i=1 ; i<=N ; i++){
+      for(int j=1 ; j<=N ; j++){
+        if(i==j) continue;
+        map[i][j] = Integer.MAX_VALUE;
       }
     }
-    for (int i = 0; i < N-1; i++) {
+    for(int i = 1; i < N; i++){
       st = new StringTokenizer(br.readLine());
       int a = Integer.parseInt(st.nextToken());
       int b = Integer.parseInt(st.nextToken());
       int c = Integer.parseInt(st.nextToken());
-      arr[a][b] = c;
-      arr[b][a] = c;
+      map[a][b] = c;
+      map[b][a] = c;
     }
-    for (int i = 0; i < M; i++) {
+    for(int i = 0; i < M; i++){
       st = new StringTokenizer(br.readLine());
       int a = Integer.parseInt(st.nextToken());
       int b = Integer.parseInt(st.nextToken());
-      twos[i][0] = a;
-      twos[i][1] = b;
+      arr[i][0] = a;
+      arr[i][1] = b;
     }
-
-  }
-  static void pro(){
-    for(int i = 1; i <= N; i++) {
-      for(int j = 1; j <= N; j++) {
-        for(int k = 1; k <= N; k++) {
-          if(arr[j][i] == Integer.MAX_VALUE || arr[i][k] == Integer.MAX_VALUE) continue;
-          arr[j][k] = Math.min(arr[j][k], arr[j][i] + arr[i][k]);
-        }
-      }
-    }
-
   }
 }
