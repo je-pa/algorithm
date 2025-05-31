7f9dfb1e84f793a15889bb69af1a83bb71481c29
je-pa <76720692+je-pa@users.noreply.github.com>
2025-05-31 11:50:56 +0900

    [Gold V] Title: 노드사이의 거리, Time: 180 ms, Memory: 21384 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -4,8 +4,10 @@ import java.io.*;
 
 public class Main {
   static int N, M;
-  static int[][] map;
+  static List<List<int[]>> lists;
   static int[][] arr;
+  static boolean[] visited;
+  static StringBuilder sb= new StringBuilder();
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,42 +16,44 @@ public class Main {
   }
 
   static void pro() {
-    for(int k = 1 ; k <= N ; k++) {
-      for(int i = 1 ; i < N ; i++){
-        for(int j = i+1 ; j <= N ; j++){
-          if(map[i][k] == Integer.MAX_VALUE) continue;
-          if(map[j][k] == Integer.MAX_VALUE) continue;
-          map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
-          map[j][i] = map[i][j];
-        }
-      }
-    }
-    StringBuilder sb= new StringBuilder();
     for(int[] a : arr){
-      sb.append(map[a[0]][a[1]]).append("\n");
+      visited = new boolean[N+1];
+      dfs(a[0],a[1], 0);
     }
     System.out.println(sb);
+  }
+
+  static void dfs(int cur, int des, int dep){
+    if(des == cur) {
+      sb.append(dep).append("\n");
+      return;
+    }
+    if(visited[cur]) return;
+    visited[cur] = true;
+
+    for(int[] arr : lists.get(cur)){
+      int next = arr[0];
+      int len = arr[1];
+      dfs(next, des, dep+len);
+    }
   }
 
   static void input(BufferedReader br) throws IOException {
     StringTokenizer st = new StringTokenizer(br.readLine());
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
-    map = new int[N+1][N+1];
+    lists = new ArrayList<>();
     arr = new int[M][2];
-    for(int i=1 ; i<=N ; i++){
-      for(int j=1 ; j<=N ; j++){
-        if(i==j) continue;
-        map[i][j] = Integer.MAX_VALUE;
-      }
+    for(int i=0 ; i<=N ; i++){
+      lists.add(new ArrayList<>());
     }
     for(int i = 1; i < N; i++){
       st = new StringTokenizer(br.readLine());
       int a = Integer.parseInt(st.nextToken());
       int b = Integer.parseInt(st.nextToken());
       int c = Integer.parseInt(st.nextToken());
-      map[a][b] = c;
-      map[b][a] = c;
+      lists.get(a).add(new int[]{b,c});
+      lists.get(b).add(new int[]{a,c});
     }
     for(int i = 0; i < M; i++){
       st = new StringTokenizer(br.readLine());
