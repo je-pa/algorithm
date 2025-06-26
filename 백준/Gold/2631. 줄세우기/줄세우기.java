7101f79bbf6f8071fe99e54300305a88f9b74f69
je-pa <76720692+je-pa@users.noreply.github.com>
2025-06-26 10:45:56 +0900

    [Gold IV] Title: 줄세우기, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.java
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -1,29 +1,38 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		
-		int[] arr = new int[N];
-		int[] dp = new int[N];
-		
-		for(int i=0;i<N;i++) {
-			arr[i] = sc.nextInt();
-		} 
-		
-		int max = 0;
-		for(int i=0;i<N;i++) {
-			dp[i] = 1;
-			for(int j=0;j<i;j++) {
-				if(arr[j]<arr[i] && dp[j]+1>dp[i]) {
-					dp[i] = dp[j]+1;
-					max = Math.max(max, dp[i]);
-				}
-			}
-		}
-		
-		System.out.println(N-max);
-	}
+  static int N;
+  static int[] arr;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    input(br);
+    pro();
+  }
+  static void pro() {
+    int[] dp = new int[N];
+    int max = 0;
+    for(int i=0;i<N;i++) {
+      dp[i] = 1;
+      for(int j=0;j<i;j++) {
+        if(arr[j]<arr[i] && dp[j]+1>dp[i]) {
+          dp[i] = dp[j]+1;
+          max = Math.max(max, dp[i]);
+        }
+      }
+    }
+    System.out.println(N-max);
+  }
+
+  private static void input(BufferedReader br) throws IOException {
+    N = Integer.parseInt(br.readLine());
+    arr = new int[N];
+    for(int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(br.readLine());
+    }
+  }
 }
+
