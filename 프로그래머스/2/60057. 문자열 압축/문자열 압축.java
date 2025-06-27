2bf48394a616a1ebd844e928e7b1329c5ff2cb48
je-pa <76720692+je-pa@users.noreply.github.com>
2025-06-27 11:01:21 +0900

    [level 2] Title: 문자열 압축, Time: 24.63 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(String s) {
+        int answer = Integer.MAX_VALUE;
+        int slen = s.length();
+        
+        for(int i=1; i<=slen ; i++){
+            answer = Math.min(dfs(s, i, 0, 0, 0, "", slen,""),answer);
+        }
+        return answer;
+    }
+    
+    public int dfs(String s, int cut, int idx, int curLen, int count, String before, int slen, String cur){
+        if(idx + cut > slen){
+            cur += (count<=1?"":count);
+            cur += before;
+            // System.out.println(cur);
+            return cur.length() + slen%cut;
+        }
+        String cs = s.substring(idx, idx+cut);
+        if(before.equals(cs)){
+            return dfs(s, cut, idx+cut, curLen, count+1, before, slen,cur);
+        }else{
+            return dfs(s, cut, idx+cut, curLen + cut, 1, cs, slen,cur+ (count<=1?"":count) + before);
+        }
+    }
+}
