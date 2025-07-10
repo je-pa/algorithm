98bf4cd64e3afe7bf27c7ee1aca5a0ff390ac2ba
je-pa <76720692+je-pa@users.noreply.github.com>
2025-07-10 11:04:48 +0900

    [Gold IV] Title: 우체국, Time: 408 ms, Memory: 44172 KB -BaekjoonHub

--- 백준/Gold/2285. 우체국/우체국.java
+++ 백준/Gold/2285. 우체국/우체국.java
@@ -7,68 +7,71 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
+  static int N;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+  public static void main(String[] args) throws NumberFormatException, IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
 
-		N = Integer.parseInt(br.readLine());
-		int ans = 0;
-		long aSum = 0L;
+    N = Integer.parseInt(br.readLine());
+    int ans = 0;
+    long aSum = 0L;
 
-		Queue<Town> queue = new PriorityQueue<>();
+    Queue<Town> queue = new PriorityQueue<>((x,y) -> {
+      return Integer.compare(x.x, y.x);
+      // return Integer.compare(x.a, y.a);
+    });
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int a = Integer.parseInt(st.nextToken());
-			aSum += (long) a;
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      int a = Integer.parseInt(st.nextToken());
+      aSum += (long) a;
 
-			queue.offer(new Town(x, a));
-		}
+      queue.offer(new Town(x, a));
+    }
 
-		if (N == 1) {
-			ans = queue.poll().x;
-		} else {
+    if (N == 1) {
+      ans = queue.poll().x;
+    } else {
 
-			long sum = 0L;
+      long sum = 0L;
 
-			for (int i = 0; i < N; i++) {
-				Town now = queue.poll();
+      for (int i = 0; i < N; i++) {
+        Town now = queue.poll();
 
-				sum += (long) now.a;
+        sum += (long) now.a;
 
-				if (sum >= (aSum + 1) / 2) {
+        if (sum >= (aSum + 1) / 2) {
 
-					ans = now.x;
-					break;
-				}
+          ans = now.x;
+          break;
+        }
 
-			}
-		}
+      }
+    }
 
-		System.out.println(ans);
+    System.out.println(ans);
 
-	}
+  }
 
-	static class Town implements Comparable<Town> {
-		int x, a;
+  static class Town {
+    int x, a;
 
-		public Town(int x, int a) {
-			this.x = x;
-			this.a = a;
-		}
+    public Town(int x, int a) {
+      this.x = x;
+      this.a = a;
+    }
 
-		@Override
-		public int compareTo(Town o) {
+    //@Override
+    //public int compareTo(Town o) {
 
-			if (this.x == o.x) {
-				return this.a - o.a;
-			} else {
-				return this.x - o.x;
-			}
-		}
-	}
+    //	if (this.x == o.x) {
+    //		return this.a - o.a;
+    //	} else {
+    //		return this.x - o.x;
+    //	}
+    //}
+  }
 
 }
