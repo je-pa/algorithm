8614d0c4ef5c47b190513b6758055631918d85d8
je-pa <76720692+je-pa@users.noreply.github.com>
2025-07-19 10:47:26 +0900

    [Gold III] Title: 코딩은 예쁘게, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.java
+++ 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.java
@@ -3,82 +3,48 @@ import java.io.*;
 
 public class Main
 {
-	public static void main (String[] args) throws IOException
-	{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int n = Integer.parseInt(br.readLine());
-		long ans = 0;
-		
-		int[] before = new int[n];
-		int[] after = new int[n];
-		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int[] count = new int[N];
+
 		st = new StringTokenizer(br.readLine());
-		for(int j = 0 ; j < n ; j++)
-		{
-			before[j] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			count[i] = Integer.parseInt(st.nextToken());
 		}
-		
+
 		st = new StringTokenizer(br.readLine());
-		for(int j = 0 ; j < n ; j++)
-		{
-			after[j] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			count[i] -= Integer.parseInt(st.nextToken());
 		}
-		
-		while(!same(before, after, n))
-		{
-			int start = 0;	//스타트 지점
-			int end = 0;
-			boolean bigger = true;
 
-			for(int i = 0 ; i < n ; i++)	//before after가 다른 시점을 찾고 탭을 삭제 / 추가해야 하는지(bigger) 표시
-			{
-				if(before[i] != after[i])
-				{
-					if(before[i] < after[i])
-						bigger = false;
-						
-					start = i;
-					end = i;
-					break;
-				}
-			}
-
-			if(start != n-1)
-			{
-				for(int i = start+1 ; i < n ; i++)	//같은 연산을 하는 끝지점을 찾아낸다
-				{
-					if(bigger && before[i] > after[i] || !bigger && before[i] < after[i]) end = i;
-					else break;
-				}
-			}
-
-			int gap = Integer.MAX_VALUE;
-			
-			for(int i = start ; i <= end ; i++)
-			{
-				gap = Math.min(gap, Math.abs(before[i] - after[i]));	//최소한의 연산 횟수 찾기
-			}
-			
-			ans += gap;
-			
-			for(int i = start ; i <= end ; i++)	//연산 실행
-			{
-				if(bigger) before[i] -= gap;
-				else before[i] += gap;
-			}
-
+		int sign, min = 0;
+		if (count[0] >= 0) {
+			sign = 1;
+		} else {
+			sign = -1;
 		}
-		System.out.println(ans);
-	}
-	public static boolean same(int[] b, int[] a, int n)
-	{
-		for(int i = 0 ; i < n ; i++)
-		{
-			if(b[i] != a[i]) return false;
+
+		int min_count = 0;
+
+		for (int i = 1; i < N; i++) {
+			if (sign * count[i] < 0) {
+				min_count = min_count + sign * (count[i - 1] - min);
+				min = 0;
+				sign = -sign;
+				continue;
+			}
+			if (sign * count[i] >= sign * count[i - 1]) {
+				continue;
+			} else {
+				min_count = min_count + sign * (count[i - 1] - min);
+				min = count[i];
+			}
 		}
-		
-		return true;
+		min_count = min_count + sign * (count[N - 1] - min);
+		System.out.println(min_count);
+
 	}
 }
